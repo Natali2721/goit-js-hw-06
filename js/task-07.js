ee9d400db5bcd1d2de56c8bc19bd289e9fd08862
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-08-14 18:10:51 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -4,3 +4,14 @@ input#font-size-control (событие input) и изменяет инлайн-
 обновляя свойство font-size. В результате при перетаскивании ползунка будет 
 меняться размер текста.
 */
+
+const refs = {
+    inputElem: document.querySelector('#font-size-control'),
+    textElem: document.querySelector('#text'),
+}
+
+refs.inputElem.addEventListener('input', event => {
+  //console.log(refs.inputElem.value);
+
+    refs.textElem.style.fontSize = `${refs.inputElem.value}px`;
+});
