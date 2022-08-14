adc65c7ac692bbafd7b9d8f14dee2936159cf9e7
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-08-14 13:33:08 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,3 +4,23 @@
 Если инпут пустой, в спане должна отображаться строка "Anonymous".
 */
 
+const inputRef = document.querySelector('#name-input');
+const nameRef = document.querySelector('#name-output');
+
+inputRef.addEventListener('input', onInputRefChange);
+
+function onInputRefChange(event) {
+
+    nameRef.textContent = event.currentTarget.value === '' ? 'Anonymous' : event.currentTarget.value;
+
+}
+
+//var2
+
+function onInputRefChangeW(event) {
+    nameRef.textContent = event.currentTarget.value;
+
+    if (nameRef.textContent.length === 0) {
+        nameRef.textContent = 'Anonymous';
+    }
+}
