af02623e93479d6d989ff6c806a584586a0a5842
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-08-14 17:59:58 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -8,3 +8,18 @@ span.color.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyElem: document.querySelector('body'),
+  btnElem: document.querySelector('.change-color'),
+  descriptionElem: document.querySelector('.color'),
+};
+
+refs.btnElem.addEventListener('click', event => {
+
+  const color = getRandomHexColor();
+  console.log(color);
+
+  refs.descriptionElem.textContent = color;
+  refs.bodyElem.style.background = color;
+});
