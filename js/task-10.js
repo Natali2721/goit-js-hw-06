b093cff3b03aff01def23dcf690aa2eedcf7e1b6
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-08-14 21:14:13 +0300

    upd colorGenerator

--- js/task-10.js
+++ js/task-10.js
@@ -20,7 +20,9 @@ div#boxes.
 */
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const refs = {
@@ -52,7 +54,7 @@ function createBoxes(amount) {
     divs.push(boxEl);
   }
 
-  refs.divBoxesElem.append(... divs);
+  refs.divBoxesElem.append(...divs);
 }
 
 function destroyBoxes() {
